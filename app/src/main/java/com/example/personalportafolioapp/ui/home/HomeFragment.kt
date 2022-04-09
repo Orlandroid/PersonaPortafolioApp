e1e374e8bde2c8a6714691ba4c7f7f1d14961f26
Orlandroid <lopez.orlando.gold9@gmail.com>
2022-04-09 18:45:08 -0500

    Add HomeView

--- app/src/main/java/com/example/personalportafolioapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/personalportafolioapp/ui/home/HomeFragment.kt
@@ -1,20 +1,39 @@
 package com.example.personalportafolioapp.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.personalportafolioapp.R
+import com.example.personalportafolioapp.databinding.FragmentHomeBinding
+import com.example.personalportafolioapp.ui.base.BaseFragment
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpUi()
     }
+
+    private fun setUpUi() {
+        with(binding) {
+            image.setAnimation(getRandomAnimation())
+            rool.tvCart.text=resources.getString(R.string.android_developer)
+            nombre.tvCart.text=resources.getString(R.string.nombre)
+            aboutMe.tvCart.text=resources.getString(R.string.about_me)
+        }
+    }
+
+    private fun getRandomAnimation():Int{
+        return when((1..4).random()){
+            1 -> R.raw.android_move
+            2 -> R.raw.android_logo
+            3 -> R.raw.android_studio_new_logo
+            else -> R.raw.kotlin_android
+        }
+    }
+
+
+
 
 }
